c4395d7d7682b4394aa0d3c518231a3f6da1d46f
yusti <SENA@CARAPRCTIFSD482.FormacionSena.RED>
2024-08-15 11:43:09 -0500

    Actualiazacion JS

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -5,47 +5,60 @@ const precioPizzaChampinion = 36000;
 const precioPizzaChicago = 68000;
 let TotalPagar = 0;
 let acumalarCantidaProductos = 0;
+let pedido = []
 function calcularFacturaEspeciales() {
   let notiPedido = document.getElementById("NotficacionPedido");
-  let btnPizzas = parseInt(document.getElementById("PizzaEsp").value);
-
-  let cantidadPizzaMar = parseInt(document.getElementById("cantidadPizzaMar").value);
-  let cantidadPizza4que = parseInt(document.getElementById("cantidadPizza4que").value);
-  let cantidadPizzaChamp = parseInt(document.getElementById("cantidadPizzaChamp").value);
-  let cantidadPizzaChi = parseInt(document.getElementById("cantidadPizzaChi").value);
+  let btnPizzas = parseInt(document.getElementById("BtnAgregarPizza").value);
 
   /*  if (btnPizzas == 1) {
     notificacionesAcumuladas = notificacionesAcumuladas + 1;
     notiPedido.innerHTML = `${notificacionesAcumuladas}`;
   } */
-
-  switch (btnPizzas) {
-    case 1:
-      notificacionesAcumuladas = notificacionesAcumuladas + 1;
-      notiPedido.innerHTML = `${notificacionesAcumuladas}`;
-      TotalPagar = cantidadPizzaMar * precioPizzaMargarita;
-      break;
-    case 2:
-      notificacionesAcumuladas = notificacionesAcumuladas + 1;
-      notiPedido.innerHTML = `${notificacionesAcumuladas}`;
-      TotalPagar = cantidadPizza4que * precioPizza4quesos;
-      break;
-    case 3:
-      notificacionesAcumuladas = notificacionesAcumuladas + 1;
-      notiPedido.innerHTML = `${notificacionesAcumuladas}`;
-      TotalPagar = cantidadPizzaChamp * precioPizzaChampinion;
-      break;
-    case 4:
-      notificacionesAcumuladas = notificacionesAcumuladas + 1;
-      notiPedido.innerHTML = `${notificacionesAcumuladas}`;
-      TotalPagar = cantidadPizzaChi * precioPizzaChicago;
-      break;
-    default:
-      break;
-  }
 }
 function verFactura() {
   let TotalFactura = document.getElementById("TotalFactura");
 
   TotalFactura.innerHTML = `${TotalPagar}`;
 }
+
+let BtnPizzaMargarita = document.getElementById("BtnPizzaMarg");
+BtnPizzaMargarita.addEventListener("click", (e) => {
+  alert("Margarita");
+  let notiPedido = document.getElementById("NotficacionPedido");
+
+  let cantidadPizzaMar = parseInt(document.getElementById("cantidadPizzaMar").value);
+  notificacionesAcumuladas = notificacionesAcumuladas + 1;
+  notiPedido.innerHTML = `${notificacionesAcumuladas}`;
+  TotalPagar = cantidadPizzaMar * precioPizzaMargarita;
+  pedido.push({id: 1 , Tipo:"Margarita", cantidad: cantidadPizzaMar ,  precio:58000 , Producto : "Pizza"  });
+});
+//
+let BtnPizza4quesos = document.getElementById("BtnPizza4quesos");
+BtnPizza4quesos.addEventListener("click", (e) => {
+  let notiPedido = document.getElementById("NotficacionPedido");
+  let cantidadPizza4que = parseInt(document.getElementById("cantidadPizza4que").value);
+  notificacionesAcumuladas = notificacionesAcumuladas + 1;
+  notiPedido.innerHTML = `${notificacionesAcumuladas}`;
+  TotalPagar = cantidadPizza4que * precioPizza4quesos;
+  pedido.push({id: 2 , Tipo:"Margarita", cantidad: cantidadPizzaMar ,  precio:46000 , Producto : "Pizza"  });
+});
+//////
+let BtnPizzaChampiniones = document.getElementById("BtnPizzaChampi");
+BtnPizzaChampiniones.addEventListener("click", (e) => {
+  let notiPedido = document.getElementById("NotficacionPedido");
+  let cantidadPizzaChamp = parseInt(document.getElementById("cantidadPizzaChamp").value);
+  notificacionesAcumuladas = notificacionesAcumuladas + 1;
+  notiPedido.innerHTML = `${notificacionesAcumuladas}`;
+  TotalPagar = cantidadPizzaChamp * precioPizzaChampinion;
+  pedido.push({id: 3 , Tipo:"Margarita", cantidad: cantidadPizzaMar ,  precio:36000 , Producto : "Pizza"  });
+});
+
+let BtnPizzaChicago = document.getElementById("BtnPizzaChicago");
+BtnPizzaChicago.addEventListener("click", (e) => {
+  let notiPedido = document.getElementById("NotficacionPedido");
+  let cantidadPizzaChi = parseInt(document.getElementById("cantidadPizzaChi").value);
+  notificacionesAcumuladas = notificacionesAcumuladas + 1;
+  notiPedido.innerHTML = `${notificacionesAcumuladas}`;
+  TotalPagar = cantidadPizzaChi * precioPizzaChicago;
+  pedido.push({id: 4 , Tipo:"Margarita", cantidad: cantidadPizzaMar ,  precio:68000 , Producto : "Pizza"  });
+});
